a652583003ae0bf4d7618d41643dd0f8e0bd05bb
An39nA0219 <an39na10282004@yahoo.co.jp>
2022-04-06 01:57:31 +0900

    ログイン機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
